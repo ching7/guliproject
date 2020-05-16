e8b9d3b0498232eca0c594fc9ae5acf74d30f910
ching7 <775608598@qq.com>
2020-05-16 11:58:59 +0800

    项目基础骨架搭建

---
+++ gulimall-order/src/test/java/con/cyn/gulimall/order/GulimallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package con.cyn.gulimall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
