9fd8d3aaa80912edc34fd0581557fb11eade302b
ching7 <775608598@qq.com>
2020-06-17 23:14:19 +0800

    add:java8特性进行商品分类父子菜单封装

--- gulimall-product/src/main/java/com/cyn/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/com/cyn/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,11 @@
 package com.cyn.gulimall.product.service.impl;
 
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -26,4 +30,50 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         return new PageUtils(page);
     }
 
+    @Override
+    public List<CategoryEntity> listWithTree() {
+        // 1、查出所有分类
+        List<CategoryEntity> entities = baseMapper.selectList(null);
+        // 2、组装成父子树形结构
+        // 2、1 找到一级分类
+        List<CategoryEntity> levelMenus = entities.stream()
+                // 找出一级
+                .filter(categoryEntity -> categoryEntity.getParentCid() == 0)
+                // 设置一级菜单子菜单
+                .map(menu -> {
+                    menu.setChildren(getChildren(menu, entities));
+                    return menu;
+                })
+                // 子菜单排序
+                .sorted((menu1, menu2) -> (menu1.getSort() == null ? 0 : menu1.getSort()) - (menu2.getSort() == null ? 0 : menu2.getSort()))
+                .collect(Collectors.toList());
+        return levelMenus;
+    }
+
+    //以下方法可以再分成逻辑处理层，减少代码耦合
+
+    /**
+     * 递归获取当前菜单子菜单
+     *
+     * @param root 当前菜单
+     * @param all  所有菜单
+     * @return
+     */
+    private List<CategoryEntity> getChildren(CategoryEntity root, List<CategoryEntity> all) {
+        List<CategoryEntity> children = all.stream()
+                .filter(categoryEntity -> {
+                    return categoryEntity.getParentCid().equals(root.getCatId());
+                })
+                // 递归找到子菜单
+                .map(categoryEntity -> {
+                    categoryEntity.setChildren(getChildren(categoryEntity, all));
+                    return categoryEntity;
+                })
+                // 菜单排序
+                .sorted((menu1, menu2) -> {
+                    return (menu1.getSort() == null ? 0 : menu1.getSort()) - (menu2.getSort() == null ? 0 : menu2.getSort());
+                })
+                .collect(Collectors.toList());
+        return children;
+    }
 }
