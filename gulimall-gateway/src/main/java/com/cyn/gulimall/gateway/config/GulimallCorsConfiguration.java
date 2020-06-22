36a359b17362e970ff45a5636120e08de9635669
ching7 <775608598@qq.com>
2020-06-22 23:50:59 +0800

    add:spring gateway的整合跨域问题解决，注释renren的跨域配置，新增自定义跨域配置。目前允许所有访问（不安全）。新增自定义路由配置

---
+++ gulimall-gateway/src/main/java/com/cyn/gulimall/gateway/config/GulimallCorsConfiguration.java
@@ -0,0 +1,27 @@
+package com.cyn.gulimall.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+/**
+ * @author chenyanan
+ * Created by chenyanan on 2020/6/22
+ */
+@Configuration
+public class GulimallCorsConfiguration {
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        //1.配置跨域
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.setAllowCredentials(true);
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsWebFilter(source);
+    }
+}
--- gulimall-product/src/main/java/com/cyn/gulimall/product/GulimalProductApplication.java
+++ gulimall-product/src/main/java/com/cyn/gulimall/product/GulimalProductApplication.java
@@ -3,6 +3,7 @@ package com.cyn.gulimall.product;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 1、整合mybatis-plus
@@ -17,6 +18,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  *
  */
+@EnableDiscoveryClient
 @MapperScan("com.cyn.gulimall.product.dao")
 @SpringBootApplication
 public class GulimalProductApplication {
