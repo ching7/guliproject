0cca3412c42605c9984fd3e6545be7220b6c7a06
chenyanan <775608598@qq.com>
2021-02-03 20:19:05 +0800

    fix:update category update

--- gulimall-product/src/main/java/com/cyn/gulimall/product/controller/CategoryController.java
+++ gulimall-product/src/main/java/com/cyn/gulimall/product/controller/CategoryController.java
@@ -62,7 +62,7 @@ public class CategoryController {
     public R info(@PathVariable("catId") Long catId) {
         CategoryEntity category = categoryService.getById(catId);
 
-        return R.ok().put("category", category);
+        return R.ok().put("data", category);
     }
 
     /**
@@ -73,6 +73,16 @@ public class CategoryController {
     public R save(@RequestBody CategoryEntity category) {
         categoryService.save(category);
 
+        return R.ok();
+    }
+
+    /**
+     * 修改
+     */
+    @RequestMapping("/update/sort")
+    //@RequiresPermissions("product:category:update")
+    public R update(@RequestBody CategoryEntity[] category) {
+        categoryService.updateBatchById(Arrays.asList(category));
         return R.ok();
     }
 
