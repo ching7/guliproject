0327e0fc66b8eb08e41be0c5a017e8b445b18aae
chenyanan <775608598@qq.com>
2021-02-04 20:34:01 +0800

    fix:oss-aliyun test

--- gulimall-product/src/test/java/com/cyn/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/cyn/gulimall/product/GulimallProductApplicationTests.java
@@ -1,5 +1,7 @@
 package com.cyn.gulimall.product;
 
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.model.PutObjectRequest;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.cyn.gulimall.product.entity.BrandEntity;
 import com.cyn.gulimall.product.service.BrandService;
@@ -7,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.File;
 import java.util.List;
 
 @SpringBootTest
@@ -14,6 +17,9 @@ class GulimallProductApplicationTests {
 
     @Autowired
     BrandService brandService;
+
+    @Autowired
+    OSSClient ossClient;
 
     @Test
     void contextLoads() {
@@ -29,6 +35,35 @@ class GulimallProductApplicationTests {
         List<BrandEntity> list = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id", 1L));
         list.forEach(e -> System.out.println(e));
         System.out.println("save success");
+    }
+
+    @Test
+    public void testUpload() {
+/*
+        // Endpoint以杭州为例，其它Region请按实际情况填写。
+        String endpoint = "oss-cn-beijing.aliyuncs.com";
+// 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
+        String accessKeyId = "LTAI4G9P5FxadKiKBBTiQTxo";
+        String accessKeySecret = "yISf3fEi8MFZd1u7LwNybDPN23MdbI";
+
+// 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+*/
+
+// 创建PutObjectRequest对象。
+        PutObjectRequest putObjectRequest = new PutObjectRequest("gulimall-chenyn", "印章.png", new File("/Users/chenyanan/Desktop/印章.png"));
+
+// 如果需要上传时设置存储类型与访问权限，请参考以下示例代码。
+// ObjectMetadata metadata = new ObjectMetadata();
+// metadata.setHeader(OSSHeaders.OSS_STORAGE_CLASS, StorageClass.Standard.toString());
+// metadata.setObjectAcl(CannedAccessControlList.Private);
+// putObjectRequest.setMetadata(metadata);
+
+// 上传文件。
+        ossClient.putObject(putObjectRequest);
+
+// 关闭OSSClient。
+        ossClient.shutdown();
     }
 
 }
